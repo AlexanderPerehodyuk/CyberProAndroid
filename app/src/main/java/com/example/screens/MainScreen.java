27e8056824b12a7e410621e166e97100ad3a1fd1
VladimirRaz <vrazzivenkov@gmail.com>
2021-09-13 20:17:43 +0500

    map-beta

--- app/src/main/java/com/example/screens/MainScreen.java
+++ app/src/main/java/com/example/screens/MainScreen.java
@@ -1,60 +1,109 @@
 package com.example.screens;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.normal.TedPermission;
+import com.yandex.mapkit.MapKitFactory;
 
 import java.util.List;
+import java.util.Locale;
 
-public class MainScreen extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity implements LocationListener {
     private LinearLayout add_problem;
     private LinearLayout map;
     private LinearLayout all_problem_list;
     private LinearLayout my_problem;
+    public static double latitude;
+    public static double longitude;
+    TextView textView_location;
+    LocationManager locationManager;
+
+    final String MAPKIT_API_KEY = "61db36cd-2c66-4ba0-a8dc-686b6a0515b8";
+
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
-        setContentView(R.layout.activity_welcome);
+        MapKitFactory.setApiKey(MAPKIT_API_KEY); // ключ апи в профиле у вовы он нужен для получения количества запроосов и д.р
+        MapKitFactory.initialize(this);
+
         Thread thread = new Thread() {
             public void run() {
                 super.run();
                 try {
-                    sleep(3000);
+                    sleep(1000); // задержка перед появлением запроса
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
-                    PermissionListener permissionlistener = new PermissionListener() {
+                    PermissionListener permissionlistener = new PermissionListener() { // создаёт окно для запроса
                         @Override
-                        public void onPermissionGranted() {
+                        public void onPermissionGranted() { // если доступ предоставили то перезод на другую активность(сейчас переходим на MainScree где у нас главное меню)
+                            getLocation();
                         }
 
                         @Override
-                        public void onPermissionDenied(List<String> deniedPermissions) {
-                            Toast.makeText(MainScreen.this, "Доступ запрещён\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+                        public void onPermissionDenied(List<String> deniedPermissions) { // если не дали разрешение то выход из приложения
+                            Toast.makeText(MainScreen.this, "Доступ запрещён\n", Toast.LENGTH_SHORT).show();
+                            finishAffinity();
                         }
 
                     };
                     TedPermission.create()
                             .setPermissionListener(permissionlistener)
-                            .setDeniedMessage("Для использования приложения нужно предоставить доступ к вашему местоположению")
-                            .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                            .setDeniedMessage("Для использования приложения нужно предоставить доступ к вашему местоположению") // текст с вопросом
+                            .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION) // как я понял, это разрешение передать в настройках местоположение для при проложения
                             .check();
                 }
             }
         };
+
         thread.start();
+        textView_location = findViewById(R.id.text_location);
+
+        //Runtime permissions
+        if (ContextCompat.checkSelfPermission(MainScreen.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainScreen.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            },100);
+        }
+
+
+//        button_location.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                getLocation();
+//            }
+//        });
+
         add_problem = findViewById(R.id.add_problem_ll);
         map = findViewById(R.id.map_ll);
         all_problem_list = findViewById(R.id.problem_ll);
@@ -70,28 +119,77 @@ public class MainScreen extends AppCompatActivity {
             /* Переход на экран картой */
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, MainScreen.class);
+                Intent intent = new Intent(MainScreen.this, MapScreen.class);
                 startActivity(intent);
-            }
-        });
-        all_problem_list.setOnClickListener(new View.OnClickListener() {
-            /*  Пока ничего, но потом должно кидать на список всех проблем */
-            @Override
-            public void onClick(View v) {
 
             }
         });
-        my_problem.setOnClickListener(new View.OnClickListener() {
-            /*  Пока ничего, но потом должно кидать на список проблем добавленных пользователем */
-            @Override
-            public void onClick(View v) {
+//        all_problem_list.setOnClickListener(new View.OnClickListener() {
+//            /*  Пока ничего, но потом должно кидать на список всех проблем */
+//            @Override
+//            public void onClick(View v) {
+//            }
+//        });
+//        my_problem.setOnClickListener(new View.OnClickListener() {
+//            /*  Пока ничего, но потом должно кидать на список проблем добавленных пользователем */
+//            @Override
+//            public void onClick(View v) {
+//            }
+//        });
+//    }
 
-            }
-        });
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//    }
+
+
+    }
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100,0,MainScreen.this);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+//        Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+        try {
+            Geocoder geocoder = new Geocoder(MainScreen.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            String address = addresses.get(0).getAddressLine(0);
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+//            textView_location.setText(latitude + "" + longitude);
+            textView_location.setText(address);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
+
+
+
--- app/src/main/java/com/example/screens/MapScreen.java
+++ app/src/main/java/com/example/screens/MapScreen.java
@@ -1,23 +1,47 @@
 package com.example.screens;
 
+import static com.example.screens.MainScreen.latitude;
+import static com.example.screens.MainScreen.longitude;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.geometry.geo.XYPoint;
+import com.yandex.mapkit.layers.GeoObjectTapEvent;
+import com.yandex.mapkit.layers.GeoObjectTapListener;
+import com.yandex.mapkit.layers.ObjectEvent;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.GeoObjectSelectionMetadata;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.InputListener;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.map.RotationType;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.places.panorama.IconMarker;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.image.ImageProvider;
 
-public class MapScreen extends AppCompatActivity {
+public class MapScreen extends AppCompatActivity implements GeoObjectTapListener, UserLocationObjectListener {
 
     private MapView mapview;
+    private UserLocationLayer userLocationLayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        MapKitFactory.setApiKey("61db36cd-2c66-4ba0-a8dc-686b6a0515b8"); // ключ апи в профиле у меня(у вовы) он нужен для получения количества запроосов и д.рю
-        MapKitFactory.initialize(this);
+
 
 
         setContentView(R.layout.activity_main);
@@ -25,11 +49,18 @@ public class MapScreen extends AppCompatActivity {
 
 
         mapview.getMap().move( // при запуске приложения переносимя на координаты которые прописаны в Point, в дальнейшем вместо них будут переменные для местоположения
-                new CameraPosition(new com.yandex.mapkit.geometry.Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
-                new Animation(Animation.Type.SMOOTH, 0),
+                new CameraPosition(new com.yandex.mapkit.geometry.Point(latitude, longitude), 17.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 1),
                 null);
+        mapview.getMap().addTapListener(this);
+        MapKit mapKit = MapKitFactory.getInstance();
+        userLocationLayer = mapKit.createUserLocationLayer(mapview.getMapWindow());
+        userLocationLayer.setVisible(true);
+//        userLocationLayer.setHeadingEnabled(true);
+//        userLocationLayer.setObjectListener(this);
 
     }
+
     // отображение карты и остановка обработки карты, когда Activity с картой становится невидимым для пользователя
     @Override
     protected void onStop() {
@@ -43,6 +74,58 @@ public class MapScreen extends AppCompatActivity {
         super.onStart();
         mapview.onStart();
         MapKitFactory.getInstance().onStart();
+    }
+
+
+    @Override
+    public boolean onObjectTap(@NonNull GeoObjectTapEvent geoObjectTapEvent) {
+        final GeoObjectSelectionMetadata selectionMetadata = geoObjectTapEvent
+                .getGeoObject()
+                .getMetadataContainer()
+                .getItem(GeoObjectSelectionMetadata.class);
+
+        if (selectionMetadata != null) {
+            mapview.getMap().selectGeoObject(selectionMetadata.getId(), selectionMetadata.getLayerId());
+
+        }
+
+        return selectionMetadata != null;
+    }
+
+
+    @Override
+    public void onObjectAdded(@NonNull UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapview.getWidth() * 0.5), (float)(mapview.getHeight() * 0.5)),
+                new PointF((float)(mapview.getWidth() * 0.5), (float)(mapview.getHeight() * 0.83)));
+
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, R.drawable.user_place));
+
+//        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+//
+//
+//
+//        pinIcon.setIcon(
+//                "pin",
+//                ImageProvider.fromResource(this, R.drawable.search_result),
+//                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+//                        .setRotationType(RotationType.ROTATE)
+//                        .setZIndex(1f)
+//                        .setScale(0.5f)
+//        );
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+    }
+
+    @Override
+    public void onObjectRemoved(@NonNull UserLocationView userLocationView) {
+
+    }
+
+    @Override
+    public void onObjectUpdated(@NonNull UserLocationView userLocationView, @NonNull ObjectEvent objectEvent) {
+
     }
 }
 
