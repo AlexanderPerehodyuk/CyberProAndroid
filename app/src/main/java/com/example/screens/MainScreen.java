b2f08d50ee3ee63f951aeba395d353d40b8878ab
razivenkov <vrazzivenkov@gmail.com>
2022-01-28 16:55:45 +0500

    недогиф.недо4

--- app/src/main/java/com/example/screens/MainScreen.java
+++ app/src/main/java/com/example/screens/MainScreen.java
@@ -6,10 +6,13 @@ import static com.example.screens.service.DATA.personalData;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -17,6 +20,8 @@ import com.example.screens.service.BaseActivity;
 import com.yandex.mapkit.MapKitFactory;
 
 public class MainScreen extends BaseActivity {
+    AnimationDrawable mDrawable;
+    ConstraintLayout mLayout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,14 +33,12 @@ public class MainScreen extends BaseActivity {
             ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 100);
 
         findViewById(R.id.add_problem_ll).setOnClickListener(new View.OnClickListener() {
-            /*  Пока ничего, но потом должно кидать на экран добваления проблемы */
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainScreen.this, AddProblem.class));
             }
         });
         findViewById(R.id.map_ll).setOnClickListener(new View.OnClickListener() {
-            /* Переход на экран картой */
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainScreen.this, MapScreen.class));
@@ -43,13 +46,16 @@ public class MainScreen extends BaseActivity {
         });
 
         findViewById(R.id.problem_all).setOnClickListener(new View.OnClickListener() {
-            /*  Пока ничего, но потом должно кидать на список всех проблем */
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainScreen.this, ProblemsActivity.class));
             }
         });
-
+        mLayout = (ConstraintLayout) findViewById(R.id.layout);
+        mDrawable = (AnimationDrawable) mLayout.getBackground();
+        mDrawable.setEnterFadeDuration(2000);
+        mDrawable.setExitFadeDuration(2000);
+        mDrawable.start();
         ((TextView) findViewById(R.id.textName)).setText((personalData[0] + " " + personalData[1]));
     }
 }
